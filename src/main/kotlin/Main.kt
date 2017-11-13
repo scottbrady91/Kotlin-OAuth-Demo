a805f39fb1f60ab89b6f040171e0b3d2f09cf4ed
Scott Brady <scottbrady91@outlook.com>
2017-11-13 21:04:32 +0000

    Either Monad

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,52 +5,71 @@ import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
 
 fun main(args: Array<String>) {
-    val response: TokenResponse = getClientCredential("http://localhost:5000/connect/token", "kotlin_oauth", "client_password", listOf("api1.read", "api1.write"))
 
-    println(response.token)
-    println(response.expiresIn)
-    println(response.tokenType)
+    val response = getClientCredential("http://localhost:5000/connect/token", "kotlin_oauth", "client_password", listOf("api1.read", "api1.write"))
+            .bind({ tokenResponse -> callApi("http://localhost:5000/api", tokenResponse.tokenType, tokenResponse.token) })
 
-    val apiResponse = callApi("http://localhost:5000/api", response.tokenType, response.token)
-
-    println(apiResponse)
+    when (response) {
+        is Response.Success -> {
+            println(response.value)
+        }
+        is Response.Failure -> {
+            println(response.error)
+        }
+    }
 }
 
-fun getClientCredential(tokenEndpoint: String, clientId: String, clientSecret: String, scopes: List<String>): TokenResponse {
+fun getClientCredential(tokenEndpoint: String, clientId: String, clientSecret: String, scopes: List<String>): Response<TokenResponse, String> {
     val (request, response, result) = tokenEndpoint.httpPost(listOf(
             "grant_type" to "client_credentials",
             "scope" to scopes.joinToString(" ")))
             .authenticate(clientId, clientSecret)
             .responseString()
 
-    when (result) {
+    return when (result) {
         is Result.Success -> {
             val parser = Parser()
             val json = parser.parse(StringBuilder(result.value)) as JsonObject
 
-            return TokenResponse(
+            Response.of(TokenResponse(
                     json["access_token"].toString(),
                     json["expires_in"].toString().toInt(),
-                    json["token_type"].toString())
+                    json["token_type"].toString()))
         }
         is Result.Failure -> {
-            // handle error
-            throw SecurityException()
+            Response.error("Token request failed")
         }
     }
 }
 
-fun callApi(apiEndpoint: String, tokenType: String, token: String): String {
+fun callApi(apiEndpoint: String, tokenType: String, token: String): Response<String, String> {
     val (request, response, result) = apiEndpoint.httpGet().header(Pair("Authorization", "$tokenType $token")).responseString()
 
     return when (result) {
         is Result.Success -> {
-            result.value
+            Response.of(result.value)
         }
         is Result.Failure -> {
-            "error!"
+            return Response.error("API request failed")
         }
     }
 }
 
 class TokenResponse(val token: String, val expiresIn: Int, val tokenType: String)
+
+sealed class Response<out L, R> {
+    class Success<out L, R>(val value: L) : Response<L, R>()
+    class Failure<out L, R>(val error: R) : Response<L, R>()
+
+    fun <X> bind(success: (L) -> (Response<X, R>)): Response<X, R> {
+        return when (this) {
+            is Response.Success<L, R> -> success(this.value)
+            is Response.Failure<L, R> -> Failure(this.error)
+        }
+    }
+
+    companion object {
+        fun <L, R> of(response: L) = Success<L, R>(response)
+        fun <L, R> error(error: R) = Failure<L, R>(error)
+    }
+}
